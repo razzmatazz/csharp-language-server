dce25abfb2a9b0e6bb9cf4c434297bf4b23b7de7
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2021-08-05 08:12:24 +0300

    merge in from fsac: src/LanguageServerProtocol/LanguageServerProtocol.fs

--- src/LanguageServerProtocol/LanguageServerProtocol.fs
+++ src/LanguageServerProtocol/LanguageServerProtocol.fs
@@ -226,6 +226,8 @@ module Types =
             /// (the server has not received an open notification before) the server can send
             /// `null` to indicate that the version is known and the content on disk is the
             /// truth (as speced with document content ownership)
+            /// Explicitly include the null value here.
+            [<JsonProperty(NullValueHandling = NullValueHandling.Include)>]
             Version: int option
         }
         interface ITextDocumentIdentifier with
@@ -321,10 +323,29 @@ module Types =
         DynamicRegistration: bool option
     }
 
+    type ResourceOperationKind = Create | Rename | Delete
+
+    type FailureHandlingKind = Abort | Transactional | Undo | TextOnlyTransactional
+
+    type ChangeAnnotationSupport = {
+      GroupsOnLabel: bool option
+    }
+
     /// Capabilities specific to `WorkspaceEdit`s
     type WorkspaceEditCapabilities = {
         /// The client supports versioned document changes in `WorkspaceEdit`s
         DocumentChanges: bool option
+        /// The resource operations the client supports. Clients should at least
+        /// support 'create', 'rename' and 'delete' files and folders.
+        ResourceOperations: ResourceOperationKind [] option
+        /// The failure handling strategy of a client if applying the workspace edit fails.
+        FailureHandling: FailureHandlingKind option
+        /// Whether the client normalizes line endings to the client specific setting.
+        /// If set to `true` the client will normalize line ending characters
+        /// in a workspace edit to the client specific new line character(s).
+        NormalizesLineEndings: bool option
+        /// Whether the client in general supports change annotations on text edits, create file, rename file and delete file changes.
+        ChangeAnnotationSupport: ChangeAnnotationSupport option
     }
 
     /// Specific capabilities for the `SymbolKind` in the `workspace/symbol` request.
