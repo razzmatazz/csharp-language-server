841c12dfb067993c22896d456931254917860a70
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2021-10-31 21:58:43 +0200

    Documentation.fs: use TripleSlashComment type to parse and reformat docxml

--- src/CSharpLanguageServer/Documentation.fs
+++ src/CSharpLanguageServer/Documentation.fs
@@ -2,80 +2,102 @@ module CSharpLanguageServer.Documentation
 
 open System
 open System.Xml.Linq
+open System.Collections.Generic
 
-type TripleSlashComment = { Lines: string list }
+type TripleSlashComment = {
+     Summary: XElement list;
+     Params: Map<string, XElement>;
+     Exceptions: Map<string, XElement>;
+     Returns: XElement list;
+     Remarks: XElement list;
+     OtherLines: XElement list; }
+
+let emptyTripleSlashComment = {
+    Summary = []
+    Params = Map.empty
+    Exceptions = Map.empty
+    Returns = []
+    Remarks = []
+    OtherLines = [] }
 
 let listOfOption o = match o with | Some v -> [v] | _ -> []
 let listOfOne i = [i]
 
-let parseToModel xmlDocumentation: TripleSlashComment =
+let formatCref (cref: string) =
+    let parts = cref.Split(':')
+    let typeName = match parts.Length with
+                    | 1 -> cref
+                    | _ -> String.Join(":", parts |> Seq.skip 1)
+    "`" + typeName + "`"
+
+let normalizeWhitespace (s: string) =
+    s.Replace("\r\n", " ").Replace("\n", " ")
+
+let formatTextElement (n: XElement) =
+
+    let formatSeeElement (e: XElement) =
+        let crefMaybe =
+            e.Attribute(XName.Get("cref"))
+            |> Option.ofObj
+            |> Option.map (fun x -> x.Value)
+            |> Option.map formatCref
+            |> listOfOption
+
+        let langWordMaybe =
+            e.Attribute(XName.Get("langword"))
+            |> Option.ofObj
+            |> Option.map (fun x -> sprintf "`%s`" x.Value)
+            |> listOfOption
+
+        crefMaybe |> Seq.append langWordMaybe |> List.ofSeq
+
+    let formatTextNode (subnode: XNode) =
+        match subnode with
+        | :? XElement as e ->
+            match e.Name.LocalName with
+            | "c" -> [sprintf "`%s`" e.Value]
+            | "see" -> formatSeeElement e
+            | "paramref" -> e.Attribute(XName.Get("name"))
+                            |> Option.ofObj
+                            |> Option.map (fun x -> sprintf "`%s`" x.Value)
+                            |> listOfOption
+            | _ -> [e.Value]
+        | :? XText as t -> t.Value |> normalizeWhitespace |> listOfOne
+        | _ -> []
+
+    n.Nodes()
+    |> Seq.collect formatTextNode
+    |> (fun ss -> String.Join("", ss))
+
+
+let extendCommentWithElement comment (n: XElement) =
+    match n.Name.LocalName with
+    | "summary" ->
+        let newSummary = comment.Summary |> List.append [n]
+        { comment with Summary = newSummary }
+
+    | "remarks" ->
+        let newRemarks = comment.Remarks |> List.append [n]
+        { comment with Remarks = newRemarks }
+
+    | "param" ->
+        let name = n.Attribute(XName.Get("name")).Value
+        { comment with Params = comment.Params |> Map.add name n }
+
+    | "returns" ->
+        { comment with Returns = comment.Returns |> List.append [n] }
+
+    | "exception" ->
+        let name = n.Attribute(XName.Get("cref")).Value |> formatCref
+        { comment with Exceptions = comment.Exceptions |> Map.add name n }
+
+    | _ ->
+        { comment with OtherLines = comment.OtherLines |> List.append [n] }
+
+
+let parseComment xmlDocumentation: TripleSlashComment =
     //printf "xmlDocumentation=%s" xmlDocumentation
     let doc = XDocument.Parse("<docroot>" + xmlDocumentation + "</docroot>")
-
-    let formatCref (cref: string) =
-        let parts = cref.Split(':')
-        let typeName = match parts.Length with
-                       | 1 -> cref
-                       | _ -> String.Join(":", parts |> Seq.skip 1)
-        "`" + typeName + "`"
-
-    let normalizeWhitespace (s: string) =
-        s.Replace("\r\n", " ").Replace("\n", " ")
-
-    let formatTextElement (n: XElement) =
-
-        let formatSeeElement (e: XElement) =
-            let crefMaybe =
-                e.Attribute(XName.Get("cref"))
-                |> Option.ofObj
-                |> Option.map (fun x -> x.Value)
-                |> Option.map formatCref
-                |> listOfOption
-
-            let langWordMaybe =
-                e.Attribute(XName.Get("langword"))
-                |> Option.ofObj
-                |> Option.map (fun x -> sprintf "`%s`" x.Value)
-                |> listOfOption
-
-            crefMaybe |> Seq.append langWordMaybe |> List.ofSeq
-
-        let formatTextSubnode (subnode: XNode) =
-            match subnode with
-            | :? XElement as e ->
-                match e.Name.LocalName with
-                | "c" -> [sprintf "`%s`" e.Value]
-                | "see" -> formatSeeElement e
-                | "paramref" -> e.Attribute(XName.Get("name"))
-                                |> Option.ofObj
-                                |> Option.map (fun x -> sprintf "`%s`" x.Value)
-                                |> listOfOption
-                | _ -> [e.Value]
-            | :? XText as t -> t.Value |> normalizeWhitespace |> listOfOne
-            | _ -> []
-
-        n.Nodes()
-        |> Seq.collect formatTextSubnode
-        |> (fun ss -> String.Join("", ss))
-
-    let elementToStrings (n: XElement) =
-        match n.Name.LocalName with
-        | "summary" -> formatTextElement n |> listOfOne
-        | "remarks" -> formatTextElement n |> listOfOne
-        | "param" ->
-            sprintf "- Param `%s`: %s"
-                    (n.Attribute(XName.Get("name")).Value)
-                    (formatTextElement n)
-            |> listOfOne
-        | "returns" ->
-            sprintf "- Returns: %s" (formatTextElement n)
-            |> listOfOne
-        | "exception" ->
-            sprintf "- Exception %s: %s"
-                    (n.Attribute(XName.Get("cref")).Value |> formatCref)
-                    (formatTextElement n)
-            |> listOfOne
-        | _ -> [n |> string] // default fallback, might not be always suitable
 
     let unwrapDocRoot (root: XElement) =
         let elementNames (el: XElement) =
@@ -85,29 +107,41 @@ let parseToModel xmlDocumentation: TripleSlashComment =
         | ["member"] -> root.Element(XName.Get("member"))
         | _ -> root
 
-    let lines =
-        doc.Root
+    doc.Root
         |> unwrapDocRoot
         |> fun r -> r.Elements()
-        |> Seq.collect elementToStrings
-        |> Seq.map (fun s -> s.Trim())
-        |> List.ofSeq
-
-    { Lines = lines }
+        |> Seq.fold extendCommentWithElement emptyTripleSlashComment
 
 
-let format model : string list =
-    model.Lines
+let formatComment model : string list =
+    let formatParamItem (item: KeyValuePair<string, XElement>) =
+        sprintf "- Param `%s`: %s" item.Key (formatTextElement item.Value)
+
+    let formatExceptionItem (item: KeyValuePair<string, XElement>) =
+        sprintf "- Exception %s: %s" item.Key (formatTextElement item.Value)
+
+    let formatReturn n =
+        sprintf "- Returns: %s" (formatTextElement n)
+
+    []
+    |> List.append (model.Summary |> List.map formatTextElement)
+    |> List.append (model.Params |> Seq.map formatParamItem |> List.ofSeq)
+    |> List.append (model.Returns |> List.map formatReturn)
+    |> List.append (model.Exceptions |> Seq.map formatExceptionItem |> List.ofSeq)
+    |> List.append (model.Remarks |> List.map formatTextElement)
+    |> List.append (model.OtherLines |> List.map string)
+    |> List.rev
+    |> List.map (fun s -> s.Trim())
 
 
 let formatDocXml xmlDocumentation =
-    let comment = parseToModel xmlDocumentation
-    String.Join("\r\n", comment.Lines)
+    let comment = parseComment xmlDocumentation
+    String.Join("\r\n", comment |> formatComment)
 
 
 let formatDocXmlWithTypeInfo xmlDocumentation typeName typeAssemblyName =
-    let comment = parseToModel xmlDocumentation
-    let formattedDocLines = format comment
+    let comment = parseComment xmlDocumentation
+    let formattedDocLines = formatComment comment
 
     let symbolInfoLines =
         match typeName, typeAssemblyName with
