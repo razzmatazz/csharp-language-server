razzmatazz/csharp-language-server

commit 1f3c8572ad2f85e29c676f6ceb01fe04977a88ef

feat(Server.fs): Support type inlay hint
Add type inlay hint to the end of identifiers, just like what clangd
does.

This commit is almost a rewrite of https://github.com/dotnet/roslyn/blob/main/src/Features/CSharp/Portable/InlineHints/CSharpInlineTypeHintsService.cs.
Because Roslyn doesn't exposes the related classes/methods (they are
`internal`), we can't call them directly. Therefore, I try to rewrite
them using F# in this commit.

Signed-off-by: Adam Tao <tcx4c70@gmail.com>

src/CSharpLanguageServer/Server.fs
@@ -11,6 +11,7 @@ open Microsoft.CodeAnalysis.FindSymbols
 open Microsoft.CodeAnalysis.Text
 open Microsoft.CodeAnalysis.Completion
 open Microsoft.CodeAnalysis.Rename
+open Microsoft.CodeAnalysis.CSharp
 open Microsoft.CodeAnalysis.CSharp.Syntax
 open Microsoft.CodeAnalysis.CodeFixes
 open Microsoft.CodeAnalysis.Classification
@@ -1121,7 +1122,71 @@ let setupServerHandlers options (lspClient: LspClient) =
         getSemanticTokensRange scope semParams.TextDocument.Uri (Some semParams.Range)
 
     let toInlayHint (semanticModel: SemanticModel) (lines: TextLineCollection) (node: SyntaxNode): InlayHint option =
-        None
+        let validateType (ty: ITypeSymbol) =
+            if isNull ty || ty :? IErrorTypeSymbol || ty.Name = "var" then
+                None
+            else
+                Some ty
+        let toTypeInlayHint (pos: int) (ty: ITypeSymbol): InlayHint =
+            { Position = pos |> lines.GetLinePosition |> lspPositionForRoslynLinePosition
+              Label = InlayHintLabel.String (": " + ty.Name)
+              Kind = Some InlayHintKind.Type
+              TextEdits = None
+              Tooltip = None
+              PaddingLeft = Some false
+              PaddingRight = Some false
+              Data = None
+            }
+
+        // It's a rewrite of https://github.com/dotnet/roslyn/blob/main/src/Features/CSharp/Portable/InlineHints/CSharpInlineTypeHintsService.cs.
+        // If Roslyn exposes the classes, then we can just do a type convert.
+        match node with
+        | :? VariableDeclarationSyntax as var when
+            var.Type.IsVar && var.Variables.Count = 1 && not var.Variables[0].Identifier.IsMissing
+            ->
+            semanticModel.GetTypeInfo(var.Type).Type
+            |> validateType
+            |> Option.map (toTypeInlayHint var.Variables[0].Identifier.Span.End)
+        | :? DeclarationExpressionSyntax as dec when
+            dec.Type.IsVar
+            ->
+            semanticModel.GetTypeInfo(dec.Type).Type
+            |> validateType
+            |> Option.map (toTypeInlayHint dec.Designation.Span.End)
+        | :? SingleVariableDesignationSyntax as var
+            ->
+            semanticModel.GetDeclaredSymbol(var)
+            |> fun sym ->
+                match sym with
+                | :? ILocalSymbol as local -> Some local
+                | _ -> None
+            |> Option.map (fun local -> local.Type)
+            |> Option.bind validateType
+            |> Option.map (toTypeInlayHint var.Identifier.Span.End)
+        | :? ForEachStatementSyntax as forEach when
+            forEach.Type.IsVar
+            ->
+            semanticModel.GetForEachStatementInfo(forEach).ElementType
+            |> validateType
+            |> Option.map (toTypeInlayHint forEach.Identifier.Span.End)
+        | :? ParameterSyntax as parameterNode when
+            isNull parameterNode.Type
+            ->
+            let parameter = semanticModel.GetDeclaredSymbol(parameterNode)
+            parameter
+            |> fun parameter ->
+                match parameter.ContainingSymbol with
+                | :? IMethodSymbol as methSym when methSym.MethodKind = MethodKind.AnonymousFunction -> Some parameter
+                | _ -> None
+            |> Option.map (fun parameter -> parameter.Type)
+            |> Option.bind validateType
+            |> Option.map (toTypeInlayHint parameterNode.Identifier.Span.End)
+        | :? ImplicitObjectCreationExpressionSyntax as implicitNew
+            ->
+            semanticModel.GetTypeInfo(implicitNew).Type
+            |> validateType
+            |> Option.map (toTypeInlayHint implicitNew.NewKeyword.Span.End)
+        | _ -> None
 
     let handleTextDocumentInlayHint (scope: ServerRequestScope) (inlayHintParams: InlayHintParams): AsyncLspResult<InlayHint [] option> = async {
         match scope.GetUserDocumentForUri inlayHintParams.TextDocument.Uri with

commit 9462bc332e4f059f756eff746f658ecb468379cb

fix(Server.fs): Avoid empty type inlay hint label
According to microsoft doc [1], `ISymbol.Name` property might be a empty
string, which leads to empty inlay hints for some types, e.g., list of
strings. Replacing it with `ISymbol.ToDisplayParts` to avoid empty inlay
hint labels.

[1] https://learn.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.isymbol.name?view=roslyn-dotnet-4.3.0#microsoft-codeanalysis-isymbol-name

Fixes: 1f3c8572ad2f ("feat(Server.fs): Support type inlay hint")
Signed-off-by: Adam Tao <tcx4c70@gmail.com>

src/CSharpLanguageServer/Server.fs
@@ -1128,9 +1128,12 @@ let setupServerHandlers options (lspClient: LspClient) =
                 None
             else
                 Some ty
+        let typeDisplayStyle = SymbolDisplayFormat(
+            genericsOptions = SymbolDisplayGenericsOptions.IncludeTypeParameters,
+            miscellaneousOptions = (SymbolDisplayMiscellaneousOptions.AllowDefaultLiteral ||| SymbolDisplayMiscellaneousOptions.IncludeNullableReferenceTypeModifier ||| SymbolDisplayMiscellaneousOptions.UseSpecialTypes))
         let toTypeInlayHint (pos: int) (ty: ITypeSymbol): InlayHint =
             { Position = pos |> lines.GetLinePosition |> lspPositionForRoslynLinePosition
-              Label = InlayHintLabel.String (": " + ty.Name)
+              Label = InlayHintLabel.String (": " + ty.ToDisplayString(typeDisplayStyle))
               Kind = Some InlayHintKind.Type
               TextEdits = None
               Tooltip = None