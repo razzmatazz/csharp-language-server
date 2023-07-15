e2973623eb2cc04b6feaf5e6cc4bb113025a2d7e
Adam Tao <tcx4c70@gmail.com>
2023-07-15 23:38:11 +0800

    feat: Implement textDocument/codeLens & codeLens/resolve
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Common/Util.fs
+++ src/Common/Util.fs
@@ -47,3 +47,7 @@ module Range =
         LinePositionSpan(
             Position.toLinePosition lines range.Start,
             Position.toLinePosition lines range.End)
+
+    let fromLinePositionSpan (pos: LinePositionSpan): Range =
+        { Start = Position.fromLinePosition pos.Start
+          End = Position.fromLinePosition pos.End }
--- src/Handlers/CodeLens.fs
+++ src/Handlers/CodeLens.fs
@@ -1,14 +1,166 @@
 namespace CSharpLanguageServer.Handlers
 
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open Microsoft.CodeAnalysis.FindSymbols
+open Microsoft.CodeAnalysis.Text
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
+open Newtonsoft.Json.Linq
 
+open CSharpLanguageServer.Common
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
+
+type private DocumentSymbolCollectorForCodeLens(semanticModel: SemanticModel) =
+    inherit CSharpSyntaxWalker(SyntaxWalkerDepth.Token)
+
+    let mutable collectedSymbols = []
+
+    let collect (node: SyntaxNode) (nameSpan: TextSpan) =
+        let symbol = semanticModel.GetDeclaredSymbol(node)
+        collectedSymbols <- (symbol, nameSpan) :: collectedSymbols
+
+    member __.GetSymbols() =
+        collectedSymbols |> List.rev |> Array.ofList
+
+    override __.VisitEnumDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitEnumDeclaration(node)
+
+    override __.VisitEnumMemberDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitEnumMemberDeclaration(node)
+
+    override __.VisitClassDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitClassDeclaration(node)
+
+    override __.VisitRecordDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitRecordDeclaration(node)
+
+    override __.VisitStructDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitStructDeclaration(node)
+
+    override __.VisitInterfaceDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitInterfaceDeclaration(node)
+
+    override __.VisitDelegateDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitDelegateDeclaration(node)
+
+    override __.VisitConstructorDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitConstructorDeclaration(node)
+
+    override __.VisitDestructorDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitDestructorDeclaration(node)
+
+    override __.VisitOperatorDeclaration(node) =
+        collect node node.OperatorToken.Span
+        base.VisitOperatorDeclaration(node)
+
+    override __.VisitIndexerDeclaration(node) =
+        collect node node.ThisKeyword.Span
+        base.VisitIndexerDeclaration(node)
+
+    override __.VisitConversionOperatorDeclaration(node) =
+        collect node node.Type.Span
+        base.VisitConversionOperatorDeclaration(node)
+
+    override __.VisitMethodDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitMethodDeclaration(node)
+
+    override __.VisitPropertyDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitPropertyDeclaration(node)
+
+    override __.VisitVariableDeclarator(node) =
+        let grandparent =
+            node.Parent
+            |> Option.ofObj
+            |> Option.bind (fun node -> node.Parent |> Option.ofObj)
+        // Only show field variables and ignore local variables
+        if grandparent.IsSome && grandparent.Value :? FieldDeclarationSyntax then
+            collect node node.Identifier.Span
+            base.VisitVariableDeclarator(node)
+        else
+            base.VisitVariableDeclarator(node)
+
+    override __.VisitEventDeclaration(node) =
+        collect node node.Identifier.Span
+        base.VisitEventDeclaration(node)
+
 
 [<RequireQualifiedAccess>]
 module CodeLens =
-    let provider: CodeLensOptions option = None
+    type CodeLensData =
+        { DocumentUri: string
+          Position: Position }
+        static member Default =
+            { DocumentUri = ""
+              Position = { Line = 0; Character = 0 } }
 
-    let handle (wm: IWorkspaceManager) (p: CodeLensParams) : AsyncLspResult<CodeLens[] option> = notImplemented
+    let provider: CodeLensOptions option = Some { ResolveProvider = Some true }
 
-    let resolve (wm: IWorkspaceManager) (p: CodeLens) : AsyncLspResult<CodeLens> = notImplemented
+    let handle (wm: IWorkspaceManager) (p: CodeLensParams) : AsyncLspResult<CodeLens[] option> = async {
+        match wm.GetDocument p.TextDocument.Uri with
+        | None -> return None |> success
+        | Some doc ->
+            let! semanticModel = doc.GetSemanticModelAsync() |> Async.AwaitTask
+            let! syntaxTree = doc.GetSyntaxTreeAsync() |> Async.AwaitTask
+            let! docText = doc.GetTextAsync() |> Async.AwaitTask
+
+            let collector = DocumentSymbolCollectorForCodeLens(semanticModel)
+            collector.Visit(syntaxTree.GetRoot())
+
+            let makeCodeLens (_symbol: ISymbol, nameSpan: TextSpan) : CodeLens =
+                let start = nameSpan.Start |> docText.Lines.GetLinePosition
+
+                let lensData: CodeLensData =
+                    { DocumentUri = p.TextDocument.Uri
+                      Position = start |> Position.fromLinePosition }
+
+                { Range = nameSpan |> docText.Lines.GetLinePositionSpan |> Range.fromLinePositionSpan
+                  Command = None
+                  Data = lensData |> JToken.FromObject |> Some }
+
+            let codeLens = collector.GetSymbols() |> Seq.map makeCodeLens
+
+            return codeLens |> Array.ofSeq |> Some |> success
+    }
+
+    let resolve (wm: IWorkspaceManager) (p: CodeLens) : AsyncLspResult<CodeLens> = async {
+        let lensData =
+            p.Data
+            |> Option.map (fun t -> t.ToObject<CodeLensData>())
+            |> Option.defaultValue CodeLensData.Default
+
+        match wm.GetDocument lensData.DocumentUri with
+        | None -> return p |> success
+        | Some doc ->
+            let! sourceText = doc.GetTextAsync() |> Async.AwaitTask
+            let position =
+                lensData.Position
+                |> Position.toLinePosition sourceText.Lines
+                |> sourceText.Lines.GetPosition
+            let! symbol = SymbolFinder.FindSymbolAtPositionAsync(doc, position) |> Async.AwaitTask
+            let! refs =
+                SymbolFinder.FindReferencesAsync(symbol, doc.Project.Solution)
+                |> Async.AwaitTask
+            let refNum = refs |> Seq.map (fun r -> r.Locations |> Seq.length) |> Seq.fold (+) 0
+            let title = sprintf "%d Reference(s)" refNum
+
+            let command =
+                { Title = title
+                  Command = "csharp.showReferences"
+                  // TODO: we really want to pass some more info to the client
+                  Arguments = None }
+
+            return { p with Command = Some command } |> success
+    }
