da773ad7678a52b403ef5627e715f0026d598c78
Adam Tao <tcx4c70@gmail.com>
2023-07-16 20:02:16 +0800

    fix: Conversion from path to URI error on Windows
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Common/Util.fs
+++ src/Common/Util.fs
@@ -6,13 +6,19 @@ open Microsoft.CodeAnalysis.Text
 
 
 module Uri =
-    let toPath (uri: string) = Uri.UnescapeDataString(Uri(uri).LocalPath)
+    // Unescape some necessary char before passing string to Uri.
+    // Can't use Uri.UnescapeDataString here. For example, if uri is "file:///z%3a/src/c%23/ProjDir" ("%3a" is
+    // ":" and "%23" is "#"), Uri.UnescapeDataString will unescape both "%3a" and "%23". Then Uri will think
+    /// "#/ProjDir" is Fragment instead of part of LocalPath.
+    let private unescape (uri: string) = uri.Replace("%3a", ":", true, null)
+
+    let toPath (uri: string) = Uri.UnescapeDataString(Uri(unescape(uri)).LocalPath)
 
     let fromPath (path: string) = Uri(path).ToString()
 
     let toWorkspaceFolder(uri: string): WorkspaceFolder =
         { Uri = uri
-          Name = Uri.UnescapeDataString(Uri(uri).Segments |> Array.last) }
+          Name = Uri.UnescapeDataString(Uri(unescape(uri)).Segments |> Array.last) }
 
 
 module Path =
