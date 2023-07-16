c595213cbc769a07304477af748151cd2246384a
Adam Tao <tcx4c70@gmail.com>
2023-07-16 20:12:39 +0800

    feat: Implement textDocument/references
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/References.fs
+++ src/Handlers/References.fs
@@ -1,12 +1,25 @@
 namespace CSharpLanguageServer.Handlers
 
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
+open CSharpLanguageServer.Common
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module References =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: ReferenceParams) : AsyncLspResult<Location[] option> = notImplemented
+    let handle (wm: IWorkspaceManager) (p: ReferenceParams) : AsyncLspResult<Location[] option> = async {
+        match! wm.FindSymbol p.TextDocument.Uri p.Position with
+        | None -> return None |> success
+        | Some symbol ->
+            let! refs = wm.FindReferences symbol
+            return
+                refs
+                |> Seq.collect (fun r -> r.Locations)
+                |> Seq.map (fun rl -> Location.fromRoslynLocation rl.Location)
+                |> Seq.toArray
+                |> Some
+                |> success
+    }
