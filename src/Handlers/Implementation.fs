5c2f830e12dfd79cdf6c7d9c66c0c1e3bae94864
Adam Tao <tcx4c70@gmail.com>
2023-07-16 19:23:13 +0800

    feat: Implement textDocument/implementation
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/Implementation.fs
+++ src/Handlers/Implementation.fs
@@ -1,13 +1,25 @@
 namespace CSharpLanguageServer.Handlers
 
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module Implementation =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<GotoResult option> =
-        notImplemented
+    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<GotoResult option> = async {
+        match! wm.FindSymbol p.TextDocument.Uri p.Position with
+        | None -> return None |> success
+        | Some symbol ->
+            let! impls = wm.FindImplementations symbol
+            let! locations = impls |> Seq.map wm.ResolveSymbolLocations |> Async.Parallel
+
+            return
+                locations
+                |> Array.collect List.toArray
+                |> GotoResult.Multiple
+                |> Some
+                |> success
+    }
