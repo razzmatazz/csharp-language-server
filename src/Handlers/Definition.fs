c762f112dff3cb895d75dbf8637e72856e8c005f
Adam Tao <tcx4c70@gmail.com>
2023-07-16 19:13:36 +0800

    feat: Implement textDocument/definition
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/Definition.fs
+++ src/Handlers/Definition.fs
@@ -1,13 +1,23 @@
 namespace CSharpLanguageServer.Handlers
 
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module Definition =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<GotoResult option> =
-        notImplemented
+    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<GotoResult option> = async {
+        match! wm.FindSymbol p.TextDocument.Uri p.Position with
+        | None -> return None |> success
+        | Some symbol ->
+            let! locations = wm.ResolveSymbolLocations symbol
+            return
+                locations
+                |> Array.ofList
+                |> GotoResult.Multiple
+                |> Some
+                |> success
+    }
