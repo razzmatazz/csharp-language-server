64d537174096c2802db2070726183359a97dea93
Adam Tao <tcx4c70@gmail.com>
2023-07-17 23:44:02 +0800

    feat: Implement textDocument/hover
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/Hover.fs
+++ src/Handlers/Hover.fs
@@ -1,12 +1,23 @@
 namespace CSharpLanguageServer.Handlers
 
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module Hover =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<Hover option> = notImplemented
+    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<Hover option> = async {
+        match! wm.FindSymbol' p.TextDocument.Uri p.Position with
+        | None -> return None |> success
+        | Some (symbol, doc) ->
+            let! semanticModel = doc.GetSemanticModelAsync() |> Async.AwaitTask
+            let content = DocumentationUtil.markdownDocForSymbolWithSignature symbol semanticModel |> markdown
+            let hover =
+                { Contents = MarkupContent content
+                  // TODO: Support range
+                  Range = None }
+            return hover |> Some |> success
+    }
