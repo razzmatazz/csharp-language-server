fc23f6826a2c3731d35c67b7eb24b41ab62d8e74
Adam Tao <tcx4c70@gmail.com>
2023-07-18 20:41:18 +0800

    feat: Implement workspace/symbol
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/WorkspaceSymbol.fs
+++ src/Handlers/WorkspaceSymbol.fs
@@ -1,13 +1,31 @@
 namespace CSharpLanguageServer.Handlers
 
+open System
+open Microsoft.CodeAnalysis
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
+open CSharpLanguageServer.Common
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module WorkspaceSymbol =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: WorkspaceSymbolParams) : AsyncLspResult<SymbolInformation[] option> =
-        notImplemented
+    let handle (wm: IWorkspaceManager) (p: WorkspaceSymbolParams) : AsyncLspResult<SymbolInformation[] option> = async {
+        let pattern =
+            if String.IsNullOrEmpty(p.Query) then
+                None
+            else
+                Some p.Query
+        let! symbols = wm.FindSymbols pattern
+        return
+            symbols
+            |> Seq.map (SymbolInformation.fromSymbol SymbolDisplayFormat.MinimallyQualifiedFormat)
+            |> Seq.collect id
+            // TODO: make 100 configurable?
+            |> Seq.truncate 100
+            |> Seq.toArray
+            |> Some
+            |> success
+    }
