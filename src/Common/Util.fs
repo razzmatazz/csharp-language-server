08efb74e370f200c2fba48bece1f848a5f642053
Adam Tao <tcx4c70@gmail.com>
2023-07-19 20:45:19 +0800

    feat: Implement textDocument/didOpen & textDocument/didClose
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Common/Util.fs
+++ src/Common/Util.fs
@@ -14,7 +14,7 @@ module Uri =
     // Can't use Uri.UnescapeDataString here. For example, if uri is "file:///z%3a/src/c%23/ProjDir" ("%3a" is
     // ":" and "%23" is "#"), Uri.UnescapeDataString will unescape both "%3a" and "%23". Then Uri will think
     /// "#/ProjDir" is Fragment instead of part of LocalPath.
-    let private unescape (uri: string) = uri.Replace("%3a", ":", true, null)
+    let unescape (uri: string) = uri.Replace("%3a", ":", true, null)
 
     let toPath (uri: string) = Uri.UnescapeDataString(Uri(unescape(uri)).LocalPath)
 
