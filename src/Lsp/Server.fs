ce5148abc538a6b6c5ba4fe287705060a40d0206
Adam Tao <tcx4c70@gmail.com>
2023-07-19 20:58:29 +0800

    refactor: Simplify (>->) via konst (const)
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Lsp/Server.fs
+++ src/Lsp/Server.fs
@@ -21,7 +21,7 @@ module Operators =
     // There is no such operator (with type Monad<'a> -> Monad<'b> -> Monad<'b>) in FSharpPlus like `>>` in
     // Haskell, so just write one.
     // In F#, `>>` is function composition, that is why we use another operator.
-    let inline (>->) (ma: '``Monad<'a>``) (mb: '``Monad<'b>``): '``Monad<'b>`` = ma >>= (fun _ -> mb)
+    let inline (>->) (ma: '``Monad<'a>``) (mb: '``Monad<'b>``): '``Monad<'b>`` = ma >>= konst mb
 
 
 type CSharpLspServer(lspClient: ICSharpLspClient, workspaceManager: IWorkspaceManager) =
