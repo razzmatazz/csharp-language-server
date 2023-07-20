739164b00e843445cae8da5bb9ea30f490bd1f47
Adam Tao <tcx4c70@gmail.com>
2023-07-20 21:49:46 +0800

    feat: Implement foramtting
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/DocumentFormatting.fs
+++ src/Handlers/DocumentFormatting.fs
@@ -1,12 +1,21 @@
 namespace CSharpLanguageServer.Handlers
 
+open Microsoft.CodeAnalysis.Formatting
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module DocumentFormatting =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: DocumentFormattingParams) : AsyncLspResult<TextEdit[] option> = notImplemented
+    let handle (wm: IWorkspaceManager) (p: DocumentFormattingParams) : AsyncLspResult<TextEdit[] option> = async {
+        match wm.GetDocument p.TextDocument.Uri with
+        | None -> return None |> success
+        | Some doc ->
+            let options = FormatUtil.getFormattingOptions doc p.Options
+            let! newDoc = Formatter.FormatAsync(doc, options) |> Async.AwaitTask
+            let! textEdits = FormatUtil.getChanges newDoc doc
+            return textEdits |> Some |> success
+    }
