6701b9a2b7b2cdd72c8167ef5cec1a7078cf2969
Adam Tao <tcx4c70@gmail.com>
2023-07-20 21:49:46 +0800

    feat: Implement foramtting
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/DocumentOnTypeFormatting.fs
+++ src/Handlers/DocumentOnTypeFormatting.fs
@@ -1,13 +1,38 @@
 namespace CSharpLanguageServer.Handlers
 
+open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.Formatting
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
+open CSharpLanguageServer.Common
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module DocumentOnTypeFormatting =
-    let provider: DocumentOnTypeFormattingOptions option = None
+    let provider: DocumentOnTypeFormattingOptions option =
+        Some
+            { FirstTriggerCharacter = ';'
+              MoreTriggerCharacter = Some([| '}'; ')' |]) }
 
-    let handle (wm: IWorkspaceManager) (p: DocumentOnTypeFormattingParams) : AsyncLspResult<TextEdit[] option> =
-        notImplemented
+    let handle (wm: IWorkspaceManager) (p: DocumentOnTypeFormattingParams) : AsyncLspResult<TextEdit[] option> = async {
+        match wm.GetDocument p.TextDocument.Uri with
+        | None -> return None |> success
+        | Some doc ->
+            let options = FormatUtil.getFormattingOptions doc p.Options
+            let! sourceText = doc.GetTextAsync() |> Async.AwaitTask
+            let pos = Position.toRoslynPosition sourceText.Lines p.Position
+
+            match p.Ch with
+            | ';'
+            | '}'
+            | ')' ->
+                let! root = doc.GetSyntaxRootAsync() |> Async.AwaitTask
+                match FormatUtil.findFormatTarget root pos with
+                | None -> return None |> success
+                | Some node ->
+                    let! newDoc = Formatter.FormatAsync(doc, TextSpan.FromBounds(node.FullSpan.Start, node.FullSpan.End), options) |> Async.AwaitTask
+                    let! textEdits = FormatUtil.getChanges newDoc doc
+                    return textEdits |> Some |> success
+            | _ -> return None |> success
+    }
--- src/Handlers/DocumentRangeFormatting.fs
+++ src/Handlers/DocumentRangeFormatting.fs
@@ -1,13 +1,28 @@
 namespace CSharpLanguageServer.Handlers
 
+open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.Formatting
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
 open CSharpLanguageServer.Common.Types
-open CSharpLanguageServer.Common.LspUtil
+open CSharpLanguageServer.Common
 
 [<RequireQualifiedAccess>]
 module DocumentRangeFormatting =
-    let provider: bool option = None
+    let provider: bool option = Some true
 
-    let handle (wm: IWorkspaceManager) (p: DocumentRangeFormattingParams) : AsyncLspResult<TextEdit[] option> =
-        notImplemented
+    let handle (wm: IWorkspaceManager) (p: DocumentRangeFormattingParams) : AsyncLspResult<TextEdit[] option> = async {
+        match wm.GetDocument p.TextDocument.Uri with
+        | None -> return None |> success
+        | Some doc ->
+            let options = FormatUtil.getFormattingOptions doc p.Options
+            let! sourceText = doc.GetTextAsync() |> Async.AwaitTask
+            let startPos = Position.toRoslynPosition sourceText.Lines p.Range.Start
+            let endPos = Position.toRoslynPosition sourceText.Lines p.Range.End
+            let! syntaxTree = doc.GetSyntaxRootAsync() |> Async.AwaitTask
+            let tokenStart = syntaxTree.FindToken(startPos).FullSpan.Start
+            let! newDoc = Formatter.FormatAsync(doc, TextSpan.FromBounds(tokenStart, endPos), options) |> Async.AwaitTask
+            let! textEdits = FormatUtil.getChanges newDoc doc
+            return textEdits |> Some |> success
+    }
