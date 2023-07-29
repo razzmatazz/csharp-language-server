f4442b433c14045345080c8caa0027c60e2d8e33
Adam Tao <tcx4c70@gmail.com>
2023-07-29 16:38:07 +0800

    feat: Merge the exes in Lsp and CSharpLanguageServer
    Use "--legacy"/"-L" to swtich between them.
    
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/CSharpLanguageServer/Program.fs
+++ src/CSharpLanguageServer/Program.fs
@@ -1,11 +1,20 @@
 module CSharpLanguageServer.Program
 
 open Argu
+open System
 open System.Reflection
+open Serilog
+open Serilog.Core
+open Serilog.Events
 open State
+
+open CSharpLanguageServer.Logging
+open CSharpLanguageServer.MSBuildWorkspacePatcher
 
 [<EntryPoint>]
 let entry args =
+    MSBuildWorkspacePatcher.Patch()
+
     try
         let argParser = ArgumentParser.Create<Options.CLIArguments>(programName = "csharp-ls")
         let serverArgs = argParser.Parse args
@@ -14,24 +23,53 @@ let entry args =
             |> Option.iter (fun _ -> printfn "csharp-ls, %s"
                                              (Assembly.GetExecutingAssembly().GetName().Version |> string)
                                      exit 0)
+        let legacy =
+            serverArgs.TryGetResult(<@ Options.CLIArguments.Legacy @>)
+            |> Option.isSome
 
-        let parseLogLevel (s: string) =
-            match s.ToLowerInvariant() with
-            | "error" -> Ionide.LanguageServerProtocol.Types.MessageType.Error
-            | "warning" -> Ionide.LanguageServerProtocol.Types.MessageType.Warning
-            | "info" -> Ionide.LanguageServerProtocol.Types.MessageType.Info
-            | "log" -> Ionide.LanguageServerProtocol.Types.MessageType.Log
-            | _ -> Ionide.LanguageServerProtocol.Types.MessageType.Log
+        if legacy then
+            let parseLogLevel (s: string) =
+                match s.ToLowerInvariant() with
+                | "error" -> Ionide.LanguageServerProtocol.Types.MessageType.Error
+                | "warning" -> Ionide.LanguageServerProtocol.Types.MessageType.Warning
+                | "info" -> Ionide.LanguageServerProtocol.Types.MessageType.Info
+                | "log" -> Ionide.LanguageServerProtocol.Types.MessageType.Log
+                | _ -> Ionide.LanguageServerProtocol.Types.MessageType.Log
 
-        // default the verbosity to warning
-        let settings: ServerSettings = {
-            SolutionPath = serverArgs.TryGetResult(<@ Options.CLIArguments.Solution @>)
-            LogLevel = serverArgs.TryGetResult(<@ Options.CLIArguments.LogLevel @>)
-                       |> Option.defaultValue "log"
-                       |> parseLogLevel
-        }
+            // default the verbosity to warning
+            let settings: ServerSettings = {
+                SolutionPath = serverArgs.TryGetResult(<@ Options.CLIArguments.Solution @>)
+                LogLevel = serverArgs.TryGetResult(<@ Options.CLIArguments.LogLevel @>)
+                           |> Option.defaultValue "log"
+                           |> parseLogLevel
+            }
 
-        Server.start settings
+            Server.start settings
+        else
+            let logLevel =
+                match serverArgs.TryGetResult(<@ Options.CLIArguments.LogLevel @>) |> Option.defaultValue "log" with
+                | "error" -> LogEventLevel.Error
+                | "warning" -> LogEventLevel.Warning
+                | "info" -> LogEventLevel.Information
+                | "log" -> LogEventLevel.Verbose
+                | _ -> LogEventLevel.Verbose
+            let logConfig =
+                LoggerConfiguration()
+                    .MinimumLevel.ControlledBy(LoggingLevelSwitch(logLevel))
+                    .Enrich.FromLogContext()
+                    .WriteTo.Async(fun conf ->
+                        conf.Console(
+                            outputTemplate =
+                                "[{Timestamp:HH:mm:ss.fff} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}",
+                            // Redirect all logs to stderr since stdout is used to communicate with client.
+                            standardErrorFromLevel = Nullable<_>(LogEventLevel.Verbose),
+                            theme = Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code
+                        )
+                        |> ignore)
+
+            Log.Logger <- logConfig.CreateLogger()
+
+            CSharpLanguageServer.Lsp.Server.start CSharpLanguageServer.Lsp.CSharpLspClient CSharpLanguageServer.Workspace.WorkspaceManager
     with
     | :? ArguParseException as ex ->
         printfn "%s" ex.Message
