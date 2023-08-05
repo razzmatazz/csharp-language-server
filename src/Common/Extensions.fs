f0207754d3ef72c613f9b3942bc37bf8b756b163
Adam Tao <tcx4c70@gmail.com>
2023-08-05 15:33:08 +0800

    refactor: Simplify via Seq.tryMaxBy
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Common/Extensions.fs
+++ src/Common/Extensions.fs
@@ -9,3 +9,11 @@ type ConcurrentDictionary<'key, 'value> with
         match this.TryGetValue key with
         | true, value -> Some value
         | _ -> None
+
+
+module Seq =
+    let inline tryMaxBy (projection: 'T -> 'U) (source: 'T seq): 'T option =
+        if isNull source || Seq.isEmpty source then
+            None
+        else
+            Seq.maxBy projection source |> Some
