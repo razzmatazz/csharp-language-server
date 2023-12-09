f3e92ce9986a3db196d4bfcce4aa3adab093aa0a
Adam Tao <tcx4c70@gmail.com>
2023-12-09 20:46:14 +0800

    feat(Handlers): Implement typeDefinition
    It's based on PR https://github.com/razzmatazz/csharp-language-server/pull/123.
    
    Signed-off-by: Adam Tao <tcx4c70@gmail.com>

--- src/Handlers/TypeDefinition.fs
+++ src/Handlers/TypeDefinition.fs
@@ -1,15 +1,57 @@
 namespace CSharpLanguageServer.Handlers
 
+open System
+open FSharpPlus
+open Microsoft.CodeAnalysis
+open Ionide.LanguageServerProtocol.Server
 open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Types.LspResult
 
 open CSharpLanguageServer.Common.Types
 open CSharpLanguageServer.Common.LspUtil
 
 [<RequireQualifiedAccess>]
 module TypeDefinition =
-    let provider (clientCapabilities: ClientCapabilities option) : bool option = None
+    let private dynamicRegistration (clientCapabilities: ClientCapabilities option) : bool =
+        clientCapabilities
+        |> Option.bind (fun x -> x.TextDocument)
+        |> Option.bind (fun x -> x.TypeDefinition)
+        |> Option.bind (fun x -> x.DynamicRegistration)
+        |> Option.defaultValue false
 
-    let registration (clientCapabilities: ClientCapabilities option) : Registration option = None
+    let provider (clientCapabilities: ClientCapabilities option) : bool option =
+        match dynamicRegistration clientCapabilities with
+        | true -> None
+        | false -> Some true
 
-    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<GotoResult option> =
-        notImplemented
+    let registration (clientCapabilities: ClientCapabilities option) : Registration option =
+        match dynamicRegistration clientCapabilities with
+        | false -> None
+        | true ->
+            Some {
+                Id = Guid.NewGuid().ToString()
+                Method = "textDocument/typeDefinition"
+                RegisterOptions = { DocumentSelector = Some defaultDocumentSelector } |> serialize |> Some }
+
+    let handle (wm: IWorkspaceManager) (p: TextDocumentPositionParams) : AsyncLspResult<GotoResult option> = async {
+        match! wm.FindSymbol' p.TextDocument.Uri p.Position with
+        | None -> return None |> success
+        | Some (symbol, doc) ->
+            let typeSymbol =
+                match symbol with
+                | :? ILocalSymbol as localSymbol -> [localSymbol.Type]
+                | :? IFieldSymbol as fieldSymbol -> [fieldSymbol.Type]
+                | :? IPropertySymbol as propertySymbol -> [propertySymbol.Type]
+                | :? IParameterSymbol as parameterSymbol -> [parameterSymbol.Type]
+                | _ -> []
+            let! locations =
+                typeSymbol
+                |> map (flip wm.ResolveSymbolLocations (Some doc.Project))
+                |> Async.Parallel
+                |> map (Seq.collect id >> Seq.toArray)
+            return
+                locations
+                |> GotoResult.Multiple
+                |> Some
+                |> success
+    }
