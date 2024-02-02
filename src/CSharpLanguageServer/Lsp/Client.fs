f0ec22694eb54c5030c76dfe9d42a7dc127d33df
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2024-02-02 08:29:36 +0200

    Add src/CSharpLanguageServer/Lsp/Client.fs

---
+++ src/CSharpLanguageServer/Lsp/Client.fs
@@ -0,0 +1,41 @@
+namespace CSharpLanguageServer.Lsp
+
+open Ionide.LanguageServerProtocol
+open Ionide.LanguageServerProtocol.Types
+open Ionide.LanguageServerProtocol.Server
+
+type CSharpLspClient(sendServerNotification: ClientNotificationSender, sendServerRequest: ClientRequestSender) =
+    inherit LspClient ()
+
+    override __.WindowShowMessage(p) =
+        sendServerNotification "window/showMessage" (box p) |> Async.Ignore
+
+    override __.WindowShowMessageRequest(p) =
+        sendServerRequest.Send "window/showMessageRequest" (box p)
+
+    override __.WindowLogMessage(p) =
+        sendServerNotification "window/logMessage" (box p) |> Async.Ignore
+
+    override __.TelemetryEvent(p) =
+        sendServerNotification "telemetry/event" (box p) |> Async.Ignore
+
+    override __.ClientRegisterCapability(p) =
+        sendServerRequest.Send "client/registerCapability" (box p)
+
+    override __.ClientUnregisterCapability(p) =
+        sendServerRequest.Send "client/unregisterCapability" (box p)
+
+    override __.WorkspaceWorkspaceFolders () =
+        sendServerRequest.Send "workspace/workspaceFolders" ()
+
+    override __.WorkspaceConfiguration (p) =
+        sendServerRequest.Send "workspace/configuration" (box p)
+
+    override __.WorkspaceApplyEdit (p) =
+        sendServerRequest.Send "workspace/applyEdit" (box p)
+
+    override __.WorkspaceSemanticTokensRefresh () =
+        sendServerNotification "workspace/semanticTokens/refresh" () |> Async.Ignore
+
+    override __.TextDocumentPublishDiagnostics(p) =
+        sendServerNotification "textDocument/publishDiagnostics" (box p) |> Async.Ignore
--- src/CSharpLanguageServer/Server.fs
+++ src/CSharpLanguageServer/Server.fs
@@ -23,44 +23,9 @@ open Ionide.LanguageServerProtocol.Server
 open Ionide.LanguageServerProtocol.Types
 open CSharpLanguageServer.RoslynHelpers
 open CSharpLanguageServer.State
+open CSharpLanguageServer.Lsp
 open System.Threading.Tasks
 open CSharpLanguageServer.Util
-
-type CSharpLspClient(sendServerNotification: ClientNotificationSender, sendServerRequest: ClientRequestSender) =
-    inherit LspClient ()
-
-    override __.WindowShowMessage(p) =
-        sendServerNotification "window/showMessage" (box p) |> Async.Ignore
-
-    override __.WindowShowMessageRequest(p) =
-        sendServerRequest.Send "window/showMessageRequest" (box p)
-
-    override __.WindowLogMessage(p) =
-        sendServerNotification "window/logMessage" (box p) |> Async.Ignore
-
-    override __.TelemetryEvent(p) =
-        sendServerNotification "telemetry/event" (box p) |> Async.Ignore
-
-    override __.ClientRegisterCapability(p) =
-        sendServerRequest.Send "client/registerCapability" (box p)
-
-    override __.ClientUnregisterCapability(p) =
-        sendServerRequest.Send "client/unregisterCapability" (box p)
-
-    override __.WorkspaceWorkspaceFolders () =
-        sendServerRequest.Send "workspace/workspaceFolders" ()
-
-    override __.WorkspaceConfiguration (p) =
-        sendServerRequest.Send "workspace/configuration" (box p)
-
-    override __.WorkspaceApplyEdit (p) =
-        sendServerRequest.Send "workspace/applyEdit" (box p)
-
-    override __.WorkspaceSemanticTokensRefresh () =
-        sendServerNotification "workspace/semanticTokens/refresh" () |> Async.Ignore
-
-    override __.TextDocumentPublishDiagnostics(p) =
-        sendServerNotification "textDocument/publishDiagnostics" (box p) |> Async.Ignore
 
 let getDotnetCliVersion () : string =
     use proc = new Process()
