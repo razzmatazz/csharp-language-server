b4c639bb7a3efd9792e3848bf825aaa81b93e852
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2024-03-21 08:53:44 +0200

    CSharpLanguageServer.Handlers.Workspace: sync with rework branch
    Co-authored-by: Adam Tao <tcx4c70@gmail.com>

--- src/CSharpLanguageServer/Handlers/Workspace.fs
+++ src/CSharpLanguageServer/Handlers/Workspace.fs
@@ -13,59 +13,70 @@ open CSharpLanguageServer.RoslynHelpers
 
 [<RequireQualifiedAccess>]
 module Workspace =
-    let private dynamicRegistration (clientCapabilities: ClientCapabilities option) =
+    let dynamicRegistration (clientCapabilities: ClientCapabilities option) =
         clientCapabilities
         |> Option.bind (fun x -> x.Workspace)
         |> Option.bind (fun x -> x.DidChangeWatchedFiles)
         |> Option.bind (fun x -> x.DynamicRegistration)
-        |> Option.defaultValue true
+        |> Option.defaultValue false
 
-    let registration (clientCapabilities: ClientCapabilities option) : Registration option =
+    let registration (clientCapabilities: ClientCapabilities option): Registration option =
         match dynamicRegistration clientCapabilities with
         | false -> None
         | true ->
-            let fileChangeWatcher = { GlobPattern = U2.First "**/*.{cs,csproj,sln}"
-                                      Kind = None }
-
+            let fileSystemWatcher =
+                // TODO: Change it to U2 after Ionide.LanguageServerProtocol release a new version.
+                { GlobPattern = U2.First "**/*.{cs,csproj,sln}"
+                  Kind = Some (WatchKind.Create ||| WatchKind.Change ||| WatchKind.Delete) }
             Some
                 { Id = Guid.NewGuid().ToString()
                   Method = "workspace/didChangeWatchedFiles"
-                  RegisterOptions = { Watchers = [| fileChangeWatcher |] } |> serialize |> Some
-                }
+                  RegisterOptions = { Watchers = [| fileSystemWatcher |] } |> serialize |> Some }
+
+    let private tryReloadDocumentOnUri logMessage diagnosticsPost (scope: ServerRequestScope) uri = async {
+        match scope.GetDocumentForUriOfType UserDocument uri with
+        | Some (doc, _) ->
+            let fileText = uri |> Util.parseFileUri |> File.ReadAllText
+            let updatedDoc = SourceText.From(fileText) |> doc.WithText
+
+            scope.Emit(SolutionChange updatedDoc.Project.Solution)
+            diagnosticsPost(DocumentBacklogUpdate)
+
+        | None ->
+            let docFilePathMaybe = uri |> Util.tryParseFileUri
+            match docFilePathMaybe with
+            | Some docFilePath ->
+                // ok, this document is not on solution, register a new one
+                let fileText = docFilePath |> File.ReadAllText
+                let! newDocMaybe = tryAddDocument logMessage
+                                                    docFilePath
+                                                    fileText
+                                                    scope.Solution
+                match newDocMaybe with
+                | Some newDoc ->
+                    scope.Emit(SolutionChange newDoc.Project.Solution)
+                    diagnosticsPost(DocumentBacklogUpdate)
+                | None -> ()
+            | None -> ()
+    }
+
+    let private removeDocument diagnosticsPost (scope: ServerRequestScope) uri =
+        match scope.GetDocumentForUriOfType UserDocument uri with
+        | Some (existingDoc, _) ->
+            let updatedProject = existingDoc.Project.RemoveDocument(existingDoc.Id)
+
+            scope.Emit(SolutionChange updatedProject.Solution)
+            scope.Emit(OpenDocVersionRemove uri)
+
+            diagnosticsPost(DocumentRemoval uri)
+        | None -> ()
 
     let didChangeWatchedFiles (logMessage: Util.AsyncLogFn)
                               (diagnosticsPost: DiagnosticsEvent -> unit)
                               (scope: ServerRequestScope)
-                              (changeParams: DidChangeWatchedFilesParams)
+                              (p: DidChangeWatchedFilesParams)
             : Async<LspResult<unit>> = async {
-        let tryReloadDocumentOnUri uri = async {
-            match scope.GetDocumentForUriOfType UserDocument uri with
-            | Some (doc, _) ->
-                let fileText = uri |> Util.parseFileUri |> File.ReadAllText
-                let updatedDoc = SourceText.From(fileText) |> doc.WithText
-
-                scope.Emit(SolutionChange updatedDoc.Project.Solution)
-                diagnosticsPost(DocumentBacklogUpdate)
-
-            | None ->
-                let docFilePathMaybe = uri |> Util.tryParseFileUri
-                match docFilePathMaybe with
-                | Some docFilePath ->
-                    // ok, this document is not on solution, register a new one
-                    let fileText = docFilePath |> File.ReadAllText
-                    let! newDocMaybe = tryAddDocument logMessage
-                                                     docFilePath
-                                                     fileText
-                                                     scope.Solution
-                    match newDocMaybe with
-                    | Some newDoc ->
-                        scope.Emit(SolutionChange newDoc.Project.Solution)
-                        diagnosticsPost(DocumentBacklogUpdate)
-                    | None -> ()
-                | None -> ()
-        }
-
-        for change in changeParams.Changes do
+        for change in p.Changes do
             match Path.GetExtension(change.Uri) with
             | ".csproj" ->
                 do! logMessage "change to .csproj detected, will reload solution"
@@ -78,40 +89,29 @@ module Workspace =
             | ".cs" ->
                 match change.Type with
                 | FileChangeType.Created ->
-                    do! tryReloadDocumentOnUri change.Uri
-
+                    do! tryReloadDocumentOnUri logMessage diagnosticsPost scope change.Uri
                 | FileChangeType.Changed ->
-                    do! tryReloadDocumentOnUri change.Uri
-
+                    do! tryReloadDocumentOnUri logMessage diagnosticsPost scope change.Uri
                 | FileChangeType.Deleted ->
-                    match scope.GetDocumentForUriOfType UserDocument change.Uri with
-                    | Some (existingDoc, _) ->
-                        let updatedProject = existingDoc.Project.RemoveDocument(existingDoc.Id)
-
-                        scope.Emit(SolutionChange updatedProject.Solution)
-                        scope.Emit(OpenDocVersionRemove change.Uri)
-
-                        diagnosticsPost(DocumentRemoval change.Uri)
-                    | None -> ()
+                    do removeDocument diagnosticsPost scope change.Uri
                 | _ -> ()
 
             | _ -> ()
 
-        return LspResult.Ok()
+        return Ok()
     }
 
     let didChangeConfiguration (scope: ServerRequestScope)
                                (configParams: DidChangeConfigurationParams)
-            : Async<LspResult<unit>> =
-        async {
-            let csharpSettings =
-                configParams.Settings
-                |> deserialize<ServerSettingsDto>
-                |> (fun x -> x.csharp)
-                |> Option.defaultValue ServerSettingsCSharpDto.Default
+            : Async<LspResult<unit>> = async {
+        let csharpSettings =
+            configParams.Settings
+            |> deserialize<ServerSettingsDto>
+            |> (fun x -> x.csharp)
+            |> Option.defaultValue ServerSettingsCSharpDto.Default
 
-            let newServerSettings = { scope.State.Settings with SolutionPath = csharpSettings.solution }
-            scope.Emit(SettingsChange newServerSettings)
+        let newServerSettings = { scope.State.Settings with SolutionPath = csharpSettings.solution }
+        scope.Emit(SettingsChange newServerSettings)
 
-            return LspResult.Ok ()
-        }
+        return Ok()
+    }
