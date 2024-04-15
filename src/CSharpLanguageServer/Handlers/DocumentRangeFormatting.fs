8570917f3bbb5a634496ab4863f7aaf933520eb4
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2024-04-15 08:39:41 +0300

    CSharpLanguageServer.Handlers.DocumentRangeFormatting: Restore cancellation support

--- src/CSharpLanguageServer/Handlers/DocumentRangeFormatting.fs
+++ src/CSharpLanguageServer/Handlers/DocumentRangeFormatting.fs
@@ -40,13 +40,16 @@ module DocumentRangeFormatting =
         match scope.GetUserDocument p.TextDocument.Uri with
         | None -> return None |> success
         | Some doc ->
+            let! ct = Async.CancellationToken
             let options = FormatUtil.getFormattingOptions doc p.Options
-            let! sourceText = doc.GetTextAsync() |> Async.AwaitTask
+            let! sourceText = doc.GetTextAsync(ct) |> Async.AwaitTask
             let startPos = Position.toRoslynPosition sourceText.Lines p.Range.Start
             let endPos = Position.toRoslynPosition sourceText.Lines p.Range.End
-            let! syntaxTree = doc.GetSyntaxRootAsync() |> Async.AwaitTask
+            let! syntaxTree = doc.GetSyntaxRootAsync(ct) |> Async.AwaitTask
             let tokenStart = syntaxTree.FindToken(startPos).FullSpan.Start
-            let! newDoc = Formatter.FormatAsync(doc, TextSpan.FromBounds(tokenStart, endPos), options) |> Async.AwaitTask
+            let! newDoc =
+                Formatter.FormatAsync(doc, TextSpan.FromBounds(tokenStart, endPos), options, cancellationToken=ct)
+                |> Async.AwaitTask
             let! textEdits = FormatUtil.getChanges newDoc doc
             return textEdits |> Some |> success
     }
