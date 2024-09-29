e76e439d6c2787b02d1e32829ab7546f6191bf29
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2024-09-29 16:46:20 +0300

    test

--- tests/CSharpLanguageServer.Tests/ReferenceTests.fs
+++ tests/CSharpLanguageServer.Tests/ReferenceTests.fs
@@ -68,3 +68,88 @@ let testReferenceWorks() =
 
     let locations1: Location[] option = classFile.Request("textDocument/references", reference1Params)
     Assert.IsTrue(locations1.IsNone)
+
+
+let dotnet8WebProjectCsproj = """<Project Sdk="Microsoft.NET.Sdk.Web">
+  <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+    <Nullable>enable</Nullable>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <RootNamespace>test_csharp_web</RootNamespace>
+  </PropertyGroup>
+</Project>
+"""
+
+[<TestCase>]
+let testReferenceWorksToAspNetRazorPageReferencedValue() =
+
+    let programCsContents = """var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+app.UseRouting();
+app.MapRazorPages();
+app.Run();
+"""
+
+    let indexCshtmlContents = """@page
+@model IndexModel
+<div>@Model.Value</div>
+"""
+
+    let indexCshtmlCsContents = """using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace test_csharp_web.Pages;
+public class IndexModel : PageModel
+{
+    public string? Value { get; set; }
+    public void OnGet()
+    {
+        Value = "test";
+    }
+}
+"""
+
+    let viewImportsCshtmlContents= """@using test_csharp_web
+@namespace test_csharp_web.Pages
+@addTagHelper *, Microsoft.AspNetCore.Mvc.TagHelpers
+"""
+    let projectFiles =
+        Map.ofList [
+          ("Project/Project.csproj", dotnet8WebProjectCsproj)
+          ("Project/Program.cs", programCsContents)
+          ("Project/Pages/Index.cshtml", indexCshtmlContents)
+          ("Project/Pages/Index.cshtml.cs", indexCshtmlCsContents)
+          ("Project/Pages/_ViewImports.cshtml", viewImportsCshtmlContents)
+        ]
+
+    use client = setupServerClient defaultClientProfile projectFiles
+    client.StartAndWaitForSolutionLoad()
+
+    use indexCshtmlCsFile = client.Open("Project/Pages/Index.cshtml.cs")
+
+    //
+    // try references request at MethodA declaration on line 2
+    //
+    let reference0Params: ReferenceParams =
+        { TextDocument = { Uri = indexCshtmlCsFile.Uri }
+          Position = { Line = 4u; Character = 19u }
+          WorkDoneToken = None
+          PartialResultToken = None
+          Context = { IncludeDeclaration = false }
+        }
+
+    let locations0: Location[] option = indexCshtmlCsFile.Request("textDocument/references", reference0Params)
+
+    System.Console.WriteLine("{0}", locations0)
+
+    let expectedLocations: Location array =
+        [|
+            { Uri = indexCshtmlCsContents.Uri
+              Range = {
+                  Start = { Line = 8u; Character = 8u }
+                  End = { Line = 8u; Character = 15u }
+              }
+            }
+         |]
+
+    Assert.AreEqual(expectedLocations, locations0.Value)
