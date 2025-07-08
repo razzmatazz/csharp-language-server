8e8f69d9475213251c9f1accf5c3e1de36f3d1bc
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-07-08 14:01:22 +0300

    Add failing test case for extension method Kind

---
+++ tests/CSharpLanguageServer.Tests/TestData/testCompletionWorksForExtensionMethods/Project/Class.cs
@@ -0,0 +1,15 @@
+class Class
+{
+    public void MethodA(string arg)
+    {
+        this.
+    }
+}
+
+public static class ClassExtensions
+{
+    public static string MethodB(this Class input)
+    {
+        return "ok";
+    }
+}
