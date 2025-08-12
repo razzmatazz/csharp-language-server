8036d3cf4eac4847b1b1dcefccaedba5e0edacac
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-08-12 10:55:44 +0300

    Update completion provider a bit more (#252)
    * Make sure CompletionProvider.ResolveProvider=true
    
    * Extract memory caching code in Handlers/Completion.fs
    
    * Make thing more functional in Completion.fs

--- src/CSharpLanguageServer/Handlers/Completion.fs
+++ src/CSharpLanguageServer/Handlers/Completion.fs
@@ -20,6 +20,21 @@ module Completion =
 
     let private completionItemMemoryCache = new MemoryCache(new MemoryCacheOptions())
 
+    let private completionItemMemoryCacheSet (cacheItemId: string) roslynDoc roslynCompletionItem =
+        completionItemMemoryCache.Set<Microsoft.CodeAnalysis.Document * Microsoft.CodeAnalysis.Completion.CompletionItem>(
+            key=cacheItemId,
+            value=(roslynDoc, roslynCompletionItem),
+            absoluteExpiration=DateTimeOffset.Now.AddMinutes(5)
+        )
+        |> ignore
+
+    let private completionItemMemoryCacheGet (cacheItemId: string) : option<Microsoft.CodeAnalysis.Document * Microsoft.CodeAnalysis.Completion.CompletionItem> =
+        let mutable value = Unchecked.defaultof<obj>
+        if completionItemMemoryCache.TryGetValue(cacheItemId, &value) then
+            Some(value :?> Microsoft.CodeAnalysis.Document * Microsoft.CodeAnalysis.Completion.CompletionItem)
+        else
+            None
+
     let emptyRoslynOptionSet: Microsoft.CodeAnalysis.Options.OptionSet =
         let osEmptyOptionSetField =
             typeof<Microsoft.CodeAnalysis.Options.OptionSet>
@@ -201,34 +216,35 @@ module Completion =
                 else
                     async.Return None
 
-            let toLspCompletionList (completions: Microsoft.CodeAnalysis.Completion.CompletionList) =
-                seq {
-                    for item in completions.ItemsList do
-                        let itemId = Guid.NewGuid() |> string
+            let toLspCompletionItemsWithCacheInfo (completions: Microsoft.CodeAnalysis.Completion.CompletionList) =
+                completions.ItemsList
+                |> Seq.map (fun item -> (item, Guid.NewGuid() |> string))
+                |> Seq.map (fun (item, cacheItemId) ->
+                    let lspCompletionItem =
+                        { Ionide.LanguageServerProtocol.Types.CompletionItem.Create item.DisplayText with
+                            Kind          = item.Tags |> Seq.tryHead |> Option.map roslynTagToLspCompletion
+                            SortText      = item.SortText |> Option.ofString
+                            FilterText    = item.FilterText |> Option.ofString
+                            InsertText    = item.DisplayText |> Option.ofString
+                            Data          = cacheItemId |> serialize |> Some
+                        }
 
-                        completionItemMemoryCache.Set<Microsoft.CodeAnalysis.Document * Microsoft.CodeAnalysis.Completion.CompletionItem>(
-                            key=itemId,
-                            value=(doc, item),
-                            absoluteExpiration=DateTimeOffset.Now.AddMinutes(5)
-                        )
-                        |> ignore
+                    (lspCompletionItem, cacheItemId, doc, item))
+                |> Array.ofSeq
 
-                        yield
-                            { Ionide.LanguageServerProtocol.Types.CompletionItem.Create item.DisplayText with
-                                Kind          = item.Tags |> Seq.tryHead |> Option.map roslynTagToLspCompletion
-                                SortText      = item.SortText |> Option.ofString
-                                FilterText    = item.FilterText |> Option.ofString
-                                InsertText    = item.DisplayText |> Option.ofString
-                                Data          = itemId |> serialize |> Some
-                            }
-                }
+            let lspCompletionItemsWithCacheInfo =
+                roslynCompletions
+                |> Option.map toLspCompletionItemsWithCacheInfo
+
+            // cache roslyn completion items
+            for (_, cacheItemId, roslynDoc, roslynItem)
+                    in (lspCompletionItemsWithCacheInfo |> Option.defaultValue Array.empty) do
+                completionItemMemoryCacheSet cacheItemId roslynDoc roslynItem
 
             return
-                roslynCompletions
-                |> Option.map (fun completions ->
-                                   { IsIncomplete = true
-                                     Items = completions |> toLspCompletionList |> Seq.toArray
-                                     ItemDefaults = None })
+                lspCompletionItemsWithCacheInfo
+                |> Option.map (fun itemsWithCacheInfo -> itemsWithCacheInfo |> Array.map (fun (item, _, _, _) -> item))
+                |> Option.map (fun items -> { IsIncomplete = true; Items = items; ItemDefaults = None })
                 |> Option.map U2.C2
                 |> LspResult.success
     }
@@ -236,14 +252,8 @@ module Completion =
     let resolve (_context: ServerRequestContext) (item: CompletionItem) : AsyncLspResult<CompletionItem> = async {
         let roslynDocAndItemMaybe =
             item.Data
-            |> Option.bind deserialize
-            |> Option.bind (fun itemId ->
-                let mutable value = Unchecked.defaultof<obj>
-                if completionItemMemoryCache.TryGetValue(itemId, &value) then
-                    Some(value :?> Microsoft.CodeAnalysis.Document * Microsoft.CodeAnalysis.Completion.CompletionItem)
-                else
-                    None
-            )
+            |> Option.bind deserialize<string option>
+            |> Option.bind completionItemMemoryCacheGet
 
         match roslynDocAndItemMaybe with
         | Some (doc, roslynCompletionItem) ->
