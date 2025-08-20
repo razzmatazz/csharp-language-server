1f10e6a006c2b3d7d02cf391958b338a4edf7351
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-08-20 13:32:52 +0300

    Add debug mode (#256)
    * Directory.Packages.props: bump Argu
    
    * src/CSharpLanguageServer/Program.fs: import Options from Options.fs
    
    * src/CSharpLanguageServer/Program.fs: Add --debug arg to enable debug mode

--- src/CSharpLanguageServer/Program.fs
+++ src/CSharpLanguageServer/Program.fs
@@ -10,19 +10,37 @@ open CSharpLanguageServer.Types
 open CSharpLanguageServer.Lsp
 open CSharpLanguageServer.Logging
 
+type CLIArguments =
+    | [<AltCommandLine("-v")>] Version
+    | [<AltCommandLine("-l")>] LogLevel of level:string
+    | [<AltCommandLine("-s")>] Solution of solution:string
+    | Debug
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Version -> "display versioning information"
+                | Solution _ -> ".sln file to load (relative to CWD)"
+                | LogLevel _ -> "log level, <trace|debug|info|warning|error>; default is `info`"
+                | Debug -> "enable debug mode"
+
+
 [<EntryPoint>]
 let entry args =
     try
-        let argParser = ArgumentParser.Create<Options.CLIArguments>(programName = "csharp-ls")
+        let argParser = ArgumentParser.Create<CLIArguments>(programName = "csharp-ls")
         let serverArgs = argParser.Parse args
 
-        serverArgs.TryGetResult(<@ Options.CLIArguments.Version @>)
-            |> Option.iter (fun _ -> printfn "csharp-ls, %s"
-                                             (Assembly.GetExecutingAssembly().GetName().Version |> string)
-                                     exit 0)
+        let printVersion () =
+            printfn "csharp-ls, %s"
+                    (Assembly.GetExecutingAssembly().GetName().Version |> string)
 
-        let logLevelArg =
-            serverArgs.TryGetResult(<@ Options.CLIArguments.LogLevel @>)
+        serverArgs.TryGetResult(<@ CLIArguments.Version @>)
+            |> Option.iter (fun _ -> printVersion ();  exit 0)
+
+        let debugMode: bool =  serverArgs.Contains Debug
+
+        let logLevelArg = serverArgs.TryGetResult(<@ CLIArguments.LogLevel @>)
 
         let logLevel =
             match logLevelArg with
@@ -31,15 +49,15 @@ let entry args =
             | Some "info" -> LogLevel.Information
             | Some "debug" -> LogLevel.Debug
             | Some "trace" -> LogLevel.Trace
-            | _ -> LogLevel.Information
-
-        Logging.setupLogging logLevel
+            | _ -> if debugMode then LogLevel.Debug else LogLevel.Information
 
         let settings = {
             ServerSettings.Default with
-                SolutionPath = serverArgs.TryGetResult(<@ Options.CLIArguments.Solution @>)
+                SolutionPath = serverArgs.TryGetResult(<@ CLIArguments.Solution @>)
                 LogLevel = logLevel
         }
+
+        Logging.setupLogging settings.LogLevel
 
         Server.start settings
     with
