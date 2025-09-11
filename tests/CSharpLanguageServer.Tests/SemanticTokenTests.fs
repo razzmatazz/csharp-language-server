a988374f6740426292ad45424468fc28f4fbbb85
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-09-11 09:59:23 +0300

    Add more tests for semantic tokens

--- tests/CSharpLanguageServer.Tests/SemanticTokenTests.fs
+++ tests/CSharpLanguageServer.Tests/SemanticTokenTests.fs
@@ -2,25 +2,81 @@ module CSharpLanguageServer.Tests.SemanticTokenTests
 
 open NUnit.Framework
 open Ionide.LanguageServerProtocol.Types
-open Ionide.LanguageServerProtocol.Server
 
-open System
 open CSharpLanguageServer.Tests.Tooling
 
-// TODO: Test other kinds of constructs, i.e. class, methods, etc.
+type DecodedToken = {
+    Line: uint
+    StartChar: uint
+    Length: uint
+    TokenType: string
+    TokenModifiers: string[]
+}
+
+let decodeSemanticToken legend (semanticToken: SemanticTokens) : list<DecodedToken> =
+    let decodeTokenWithLegend (legend: SemanticTokensLegend) (token: uint[]) : DecodedToken =
+        if token.Length <> 5 then failwith "invalid size of `token`, must be 5!"
+
+        let bitIndexes (n:int) =
+            seq {
+                let mutable x = n
+                let mutable i = 0
+                while x <> 0 do
+                    if (x &&& 1) <> 0 then
+                        yield i
+                    x <- x >>> 1
+                    i <- i + 1
+            } |> Seq.toArray
+
+        let tokenModifiers =
+            token[4] |> int |> bitIndexes |> Array.map (fun i -> legend.TokenModifiers[i])
+
+        { Line = token[0]
+          StartChar = token[1]
+          Length = token[2]
+          TokenType = legend.TokenTypes[int token[3]]
+          TokenModifiers = tokenModifiers
+        }
+
+    let offsetMappingFn (prevTok: option<DecodedToken>) (tok: DecodedToken) =
+        let mappedTok =
+            match prevTok with
+            | None -> tok
+            | Some prevTok ->
+                match tok.Line with
+                | 0u -> { tok with Line = prevTok.Line
+                                   StartChar = prevTok.StartChar + tok.StartChar }
+                | _ -> { tok with Line = prevTok.Line + tok.Line }
+
+        mappedTok, Some mappedTok
+
+    let tokens, _ =
+        semanticToken.Data
+        |> Seq.chunkBySize 5
+        |> Seq.map (decodeTokenWithLegend legend)
+        |> Seq.mapFold offsetMappingFn None
+
+    tokens |> List.ofSeq
+
+
 [<TestCase>]
-let testSemanticTokenWorks () =
-    use client = setupServerClient defaultClientProfile "TestData/testSemanticToken"
+let testSemanticTokens () =
+    use client = setupServerClient defaultClientProfile "TestData/testSemanticTokens"
     client.StartAndWaitForSolutionLoad()
 
-    // Make sure the server have right capability.
-    let haveFullSemanticTokenCapability =
+    let semanticTokensOptions =
         client.GetState().ServerCapabilities
         |> Option.bind (fun c -> c.SemanticTokensProvider)
         |> Option.bind (fun s ->
             match s with
             | U2.C1 c1 -> Some c1
             | _ -> None)
+
+    Assert.IsTrue semanticTokensOptions.IsSome
+
+    // Make sure the server exposes the capability.
+    let haveFullSemanticTokenCapability =
+        semanticTokensOptions
         |> Option.bind (fun c1 -> c1.Full)
         |> Option.bind (fun f ->
             match f with
@@ -29,6 +85,45 @@ let testSemanticTokenWorks () =
         |> Option.defaultValue false
 
     Assert.IsTrue haveFullSemanticTokenCapability
+
+    use file = client.Open "Project/Program.cs"
+
+    let semanticTokenParams: SemanticTokensParams =
+        { PartialResultToken = None
+          WorkDoneToken = None
+          TextDocument = { Uri = file.Uri } }
+
+    let semanticToken: SemanticTokens =
+        client.Request("textDocument/semanticTokens/full", semanticTokenParams)
+
+    Assert.IsTrue(semanticToken.ResultId.IsNone)
+
+    let tokens =
+        semanticToken
+        |> (decodeSemanticToken semanticTokensOptions.Value.Legend)
+
+    Assert.AreEqual(123, tokens.Length)
+
+    let firstNTokens = tokens |> Seq.take 8 |> Array.ofSeq
+
+    Assert.AreEqual(
+        [|
+            { Line=0u; StartChar=0u; Length=5u; TokenType="keyword"; TokenModifiers=[||] }
+            { Line=0u; StartChar=6u; Length=6u; TokenType="namespace"; TokenModifiers=[||] }
+            { Line=2u; StartChar=0u; Length=9u; TokenType="keyword"; TokenModifiers=[||] }
+            { Line=2u; StartChar=10u; Length=7u; TokenType="namespace"; TokenModifiers=[||] }
+            { Line=4u; StartChar=4u; Length=9u; TokenType="keyword"; TokenModifiers=[||] }
+            { Line=4u; StartChar=14u; Length=10u; TokenType="interface"; TokenModifiers=[||] }
+            { Line=6u; StartChar=8u; Length=6u; TokenType="keyword"; TokenModifiers=[||] }
+            { Line=6u; StartChar=15u; Length=11u; TokenType="method"; TokenModifiers=[||] }
+         |],
+         firstNTokens)
+
+
+[<TestCase>]
+let testSemanticTokensWithMultiLineLiteral () =
+    use client = setupServerClient defaultClientProfile "TestData/testSemanticTokensWithMultiLineLiteral"
+    client.StartAndWaitForSolutionLoad()
 
     use file = client.Open "Project/Program.cs"
     let len = (file.GetFileContentsWithTextEditsApplied Array.empty).Length
@@ -47,5 +142,3 @@ let testSemanticTokenWorks () =
         |> Array.chunkBySize 5
         |> Array.fold (fun st -> fun datum -> st || datum[2] > uint32 len) false
     )
-
-    ()
---
+++ tests/CSharpLanguageServer.Tests/TestData/testSemanticTokens/Project/Program.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Project
+{
+    interface IGreetable
+    {
+        string GetGreeting();
+    }
+
+    class Person : IGreetable
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public virtual string GetGreeting()
+        {
+            return $"Hello, my name is {Name}.";
+        }
+    }
+
+    class Student : Person
+    {
+        public int Grade { get; set; }
+
+        public Student(string name, int grade) : base(name)
+        {
+            Grade = grade;
+        }
+
+        public override string GetGreeting()
+        {
+            return $"Hi, I'm {Name} and I'm in grade {Grade}.";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string normal = "C# is fun!";
+            string verbatim = @"Path with \slashes\ stays literal";
+            string interpolated = $"Today is {DateTime.Now:dddd}";
+
+            Console.WriteLine(normal);
+
+            IGreetable p1 = new Person("Alice");
+            IGreetable p2 = new Student("Bob", 10);
+
+            Console.WriteLine(p1.GetGreeting());
+
+            void Shout(string msg) => Console.WriteLine(msg.ToUpper());
+            Shout("this is a local function!");
+        }
+    }
+}
