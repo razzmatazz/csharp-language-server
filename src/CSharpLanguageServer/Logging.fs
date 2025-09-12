1e836c05081dd8ad6290e7681b2481a56f4fe56d
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-09-12 11:12:29 +0300

    Update Logging format to include timestamp

--- src/CSharpLanguageServer/Logging.fs
+++ src/CSharpLanguageServer/Logging.fs
@@ -1,20 +1,28 @@
 namespace CSharpLanguageServer.Logging
 
-open System
 open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Logging.Console
 
 module Logging =
     let mutable private loggerFactory: option<ILoggerFactory> = None
 
     let setupLogging (minimumLevel: LogLevel) =
-        let factory = LoggerFactory.Create(fun builder ->
-            builder
-                .AddConsole(fun opts ->
-                    opts.LogToStandardErrorThreshold <- LogLevel.Trace // send everything to stderr
-                )
-                .SetMinimumLevel(minimumLevel) |> ignore
-        )
 
-        loggerFactory <- Some factory
+        let createConsoleLogger (builder: ILoggingBuilder) =
+            let configureSimpleConsole (opts: SimpleConsoleFormatterOptions) =
+                opts.SingleLine <- true
+                opts.TimestampFormat <- "H:mm:ss.fff "
+                opts.IncludeScopes <- true
+
+            let configureConsoleLogger (opts: ConsoleLoggerOptions) =
+                opts.LogToStandardErrorThreshold <- LogLevel.Trace // send everything to stderr
+
+            builder
+                .AddSimpleConsole(configureSimpleConsole)
+                .AddConsole(configureConsoleLogger)
+                .SetMinimumLevel(minimumLevel)
+                |> ignore
+
+        loggerFactory <- LoggerFactory.Create(createConsoleLogger) |> Some
 
     let getLoggerByName name = loggerFactory.Value.CreateLogger name
