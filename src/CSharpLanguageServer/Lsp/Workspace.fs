b975e36a80b6aa15bb930e7bc5adfb82c69ddc7e
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-10-29 14:18:25 +0200

    chore(Lsp.Workspace): move ServerState.Solution and .RootPath under LspWorkspaceFolder

--- src/CSharpLanguageServer/Lsp/Workspace.fs
+++ src/CSharpLanguageServer/Lsp/Workspace.fs
@@ -20,3 +20,33 @@ type LspWorkspace =
     { Folders: LspWorkspaceFolder list }
 
     static member Empty = { Folders = [] }
+
+    member this.SingletonFolder = this.Folders |> Seq.exactlyOne
+
+    member this.RootPath = this.Folders |> Seq.exactlyOne |> _.Uri |> Uri.toPath
+
+    member this.Solution = this.Folders |> Seq.tryExactlyOne |> Option.bind _.Solution
+
+    member this.WithSingletonFolderUpdated(update: LspWorkspaceFolder -> LspWorkspaceFolder) =
+        let emptyFolder =
+            { Uri = Directory.GetCurrentDirectory() |> Uri.fromPath
+              Name = None
+              RoslynWorkspace = None
+              Solution = None }
+
+        let updatedFolders =
+            this.Folders
+            |> Seq.tryExactlyOne
+            |> Option.defaultValue emptyFolder
+            |> update
+            |> List.singleton
+
+        { this with Folders = updatedFolders }
+
+    member this.WithRootPath(rootPath: string) =
+        this.WithSingletonFolderUpdated(fun f ->
+            { f with
+                Uri = rootPath |> Uri.fromPath })
+
+    member this.WithSolution(solution: Solution option) =
+        this.WithSingletonFolderUpdated(fun f -> { f with Solution = solution })
