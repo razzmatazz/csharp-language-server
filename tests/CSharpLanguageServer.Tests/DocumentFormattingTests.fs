a3c077cf3d626dd72c5730089b9592aa91e6beba
Saulius Menkevičius <sauliusmenkevicius@fastmail.com>
2025-11-27 09:54:56 +0200

    chore(Tests.CodeActionTests): run code action tests w/ multiple tfms

--- tests/CSharpLanguageServer.Tests/DocumentFormattingTests.fs
+++ tests/CSharpLanguageServer.Tests/DocumentFormattingTests.fs
@@ -29,7 +29,7 @@ let testEditorConfigFormatting () =
     | Some tes ->
         let expectedClassContents =
             File
-                .ReadAllText(Path.Combine(client.ProjectDir, "Project", "Class.cs.formatted.txt"))
+                .ReadAllText(Path.Combine(client.SolutionDir, "Project", "Class.cs.formatted.txt"))
                 .ReplaceLineEndings("\n")
 
         let actualClassContents =
